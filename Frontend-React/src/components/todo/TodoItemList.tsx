03fc3ad3a8bab31a05732f028b21c6c6b5847530
Rowan Walker <rowan.walker.online@gmail.com>
2024-08-05 00:43:10 +1200

    Ran prettier

--- Frontend-React/src/components/todo/TodoItemList.tsx
+++ Frontend-React/src/components/todo/TodoItemList.tsx
@@ -1,64 +1,63 @@
-import React, { useState, useEffect } from 'react';
-import { Button, Table } from 'react-bootstrap';
-import { TodoItem } from '../../types';
+import React, { useState, useEffect } from 'react'
+import { Button, Table } from 'react-bootstrap'
+import { TodoItem } from '../../types'
 
 const TodoItemList = () => {
+  const [items, setItems] = useState([])
 
-    const [items, setItems] = useState([])
+  useEffect(() => {
+    // todo
+  }, [])
 
-    useEffect(() => {
-      // todo
-    }, [])
-  
-    async function getItems() {
-      try {
-        alert('todo')
-      } catch (error) {
-        console.error(error)
-      }
+  async function getItems() {
+    try {
+      alert('todo')
+    } catch (error) {
+      console.error(error)
     }
-  
-    async function handleMarkAsComplete(item: TodoItem) {
-      try {
-        alert('todo')
-      } catch (error) {
-        console.error(error)
-      }
+  }
+
+  async function handleMarkAsComplete(item: TodoItem) {
+    try {
+      alert('todo')
+    } catch (error) {
+      console.error(error)
     }
+  }
 
-    return (
-        <>
-        <h1>
-          Showing {items.length} Item(s){' '}
-          <Button variant="primary" className="pull-right" onClick={() => getItems()}>
-            Refresh
-          </Button>
-        </h1>
+  return (
+    <>
+      <h1>
+        Showing {items.length} Item(s){' '}
+        <Button variant="primary" className="pull-right" onClick={() => getItems()}>
+          Refresh
+        </Button>
+      </h1>
 
-        <Table striped bordered hover>
-          <thead>
-            <tr>
-              <th>Id</th>
-              <th>Description</th>
-              <th>Action</th>
+      <Table striped bordered hover>
+        <thead>
+          <tr>
+            <th>Id</th>
+            <th>Description</th>
+            <th>Action</th>
+          </tr>
+        </thead>
+        <tbody>
+          {items.map((item: TodoItem) => (
+            <tr key={item.id}>
+              <td>{item.id}</td>
+              <td>{item.description}</td>
+              <td>
+                <Button variant="warning" size="sm" onClick={() => handleMarkAsComplete(item)}>
+                  Mark as completed
+                </Button>
+              </td>
             </tr>
-          </thead>
-          <tbody>
-            {items.map((item: TodoItem) => (
-              <tr key={item.id}>
-                <td>{item.id}</td>
-                <td>{item.description}</td>
-                <td>
-                  <Button variant="warning" size="sm" onClick={() => handleMarkAsComplete(item)}>
-                    Mark as completed
-                  </Button>
-                </td>
-              </tr>
-            ))}
-          </tbody>
-        </Table>
-      </>
-    )
+          ))}
+        </tbody>
+      </Table>
+    </>
+  )
 }
 
 export default TodoItemList
--- Frontend-React/src/types/index.d.ts
+++ Frontend-React/src/types/index.d.ts
@@ -1,5 +1,5 @@
 export interface TodoItem {
-    id: number
-    description: string
-    completed: boolean
-  }
+  id: number
+  description: string
+  completed: boolean
+}
